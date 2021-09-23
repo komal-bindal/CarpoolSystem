ac23810b30dc362a2c3686ed91e156a2a14379e8
Kritika sharma <kritika.sharma_cs19@gla.ac.in>
2021-09-23 21:34:43 +0530

    Created test cases to check the validation of email-id,phone number and password.

--- app/src/test/java/com/example/carpoolsystem/RegistrationUtilsTest.kt
+++ app/src/test/java/com/example/carpoolsystem/RegistrationUtilsTest.kt
@@ -58,4 +58,84 @@ class RegistrationUtilsTest {
         val result = RegistrationUtils.isValidPassword("Abyt5der69hsrecdhbxd@^")
         Assert.assertFalse(result)
     }
+    @Test
+    fun isValidPhoneNumber_LengthLimitExceed_False(){
+        val result=RegistrationUtils.isValidPhoneNumber("12233456789234")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidPhoneNumber_LengthLimitLessThanRequired_False(){
+        val result=RegistrationUtils.isValidPhoneNumber("12238765")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidPhoneNumber_NumberIsNull_False(){
+        val result=RegistrationUtils.isValidPhoneNumber("")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidPhoneNumber_NumberContainsSpaces_False(){
+        val result=RegistrationUtils.isValidPhoneNumber(" ")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidPhoneNumber_NumberContainsSpecialCharacters_False(){
+        val result=RegistrationUtils.isValidPhoneNumber("123456789#")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidPhoneNumber_NumberContainsAlphabets_False(){
+        val result=RegistrationUtils.isValidPhoneNumber("675421234r")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidPhoneNumber_NumberContainsSpacesInBetween_False(){
+        val result=RegistrationUtils.isValidPhoneNumber("3214567 98")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidPhoneNumber_NumberContainsZero_False(){
+        val result=RegistrationUtils.isValidPhoneNumber("08979809698")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidPhoneNumber_NumberContainsNineOne_False(){
+        val result=RegistrationUtils.isValidPhoneNumber("+918979809698")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidEmail_EmailIsEmpty_False(){
+        val result=RegistrationUtils.isValidEmail("")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidEmail_EmailContainsSpaces_False(){
+        val result=RegistrationUtils.isValidEmail(" ")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidEmail_EmailDoesNotContainsGlaNotation_False(){
+        val result=RegistrationUtils.isValidEmail("skritika2000@gmail.com")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidEmail_EmailDoesNotContainsSpecialSymbols_False(){
+        val result=RegistrationUtils.isValidEmail("kritkika.sharma_cs19@glaac.in")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidEmail_EmailDoesNotStartWithCapitalLetter_False(){
+        val result=RegistrationUtils.isValidEmail("Kritkika.sharma_cs19@glaac.in")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidEmail_EmailDoesNotContainsCapitalLetter_False(){
+        val result=RegistrationUtils.isValidEmail("Kritkika.shaRma_cs19@glaac.in")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidEmail_EmailDoesNotConatinsUnderscore_False(){
+        val result=RegistrationUtils.isValidEmail("Kritkika.sharmacs19@gla.ac.in")
+        Assert.assertFalse(result)
+    }
 }
