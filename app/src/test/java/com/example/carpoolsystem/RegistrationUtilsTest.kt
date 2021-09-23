567bc027e65fb650129f425ab2915e0b272a6c49
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-09-23 19:55:24 +0530

    rename test functions according to convention

--- app/src/test/java/com/example/carpoolsystem/RegistrationUtilsTest.kt
+++ app/src/test/java/com/example/carpoolsystem/RegistrationUtilsTest.kt
@@ -5,54 +5,56 @@ import org.junit.Test
 
 class RegistrationUtilsTest {
     @Test
-    fun isValidPassword_SmallCaseAlphabetsOnly_false() {
+    fun isValidPassword_SmallCaseAlphabetsOnly_False() {
         val result = RegistrationUtils.isValidPassword("abcdef")
         Assert.assertFalse(result)
     }
 
     @Test
-    fun isValidPassword_CapitalCaseAlphabetsOnly_false() {
+    fun isValidPassword_CapitalCaseAlphabetsOnly_False() {
         val result = RegistrationUtils.isValidPassword("ABCDEF")
         Assert.assertFalse(result)
     }
 
     @Test
-    fun isValidPassword_LengthLessThan4_false() {
+    fun isValidPassword_LengthLessThan4_False() {
         val result = RegistrationUtils.isValidPassword("A@2")
         Assert.assertFalse(result)
     }
 
     @Test
-    fun isValidPassword_Empty_false() {
+    fun isValidPassword_Empty_False() {
         val result = RegistrationUtils.isValidPassword("")
         Assert.assertFalse(result)
     }
 
     @Test
-    fun isValidPassword_ContainsSpace_false() {
+    fun isValidPassword_ContainsSpace_False() {
         val result = RegistrationUtils.isValidPassword("A 8 u76#")//A 8 u76#
         //ade  67#
         Assert.assertFalse(result)
     }
 
     @Test
-    fun isValidPassword_DigitsOnly_false() {
+    fun isValidPassword_DigitsOnly_False() {
         var result = RegistrationUtils.isValidPassword("12345")
         Assert.assertFalse(result)
     }
 
     @Test
-    fun isValidPassword_validPassword_true() {
+    fun isValidPassword_ValidPassword_True() {
         var result = RegistrationUtils.isValidPassword("aA34@")
         Assert.assertTrue(result)
     }
+
     @Test
-    fun isValidPassword_SpecialCharactersOnly_true() {
+    fun isValidPassword_SpecialCharactersOnly_True() {
         val result = RegistrationUtils.isValidPassword("@#%^&+=@")
         Assert.assertFalse(result)
     }
+
     @Test
-    fun isValidPassword_LimitExceed_false() {
+    fun isValidPassword_LimitExceed_False() {
         val result = RegistrationUtils.isValidPassword("Abyt5der69hsrecdhbxd@^")
         Assert.assertFalse(result)
     }
