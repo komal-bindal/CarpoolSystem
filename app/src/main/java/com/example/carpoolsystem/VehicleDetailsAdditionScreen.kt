45098e556f9cecadb4ff0db8f1aa7f3d07b17724
Kritika sharma <kritika.sharma_cs19@gla.ac.in>
2021-10-01 01:07:40 +0530

    create UI for VehicleDetailsAdditionScreen.kt through activity_vehicle_details_addition_screen.xml.

---
+++ app/src/main/java/com/example/carpoolsystem/VehicleDetailsAdditionScreen.kt
@@ -0,0 +1,11 @@
+package com.example.carpoolsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VehicleDetailsAdditionScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vehicle_details_addition_screen)
+    }
+}
