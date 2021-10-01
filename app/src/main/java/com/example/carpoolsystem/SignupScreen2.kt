205928fd3409946dca123f01f62e2fcc6b8fe2df
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-10-01 19:19:56 +0530

    Add business logic for sending otp to phone number.

--- app/src/main/java/com/example/carpoolsystem/SignupScreen2.kt
+++ app/src/main/java/com/example/carpoolsystem/SignupScreen2.kt
@@ -7,26 +7,41 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.PhoneAuthProvider.ForceResendingToken
+import com.google.firebase.auth.PhoneAuthProvider.OnVerificationStateChangedCallbacks
+import java.util.concurrent.TimeUnit
+
 
 class SignupScreen2 : AppCompatActivity() {
-    private lateinit var phoneNumber: EditText
-    private lateinit var OTP: EditText
+    private lateinit var phoneNumberEditText: EditText
+    private lateinit var otpEditText: EditText
+
+    private lateinit var getOtpButton: Button
     private lateinit var verifyButton: Button
+    private lateinit var verificationOTP: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val intent: Intent
-        intent = Intent(this, SignupScreen1::class.java)
 
+        super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_signup_screen2)
-        phoneNumber = findViewById(R.id.editTextPhoneNumber)
-        OTP = findViewById(R.id.editTextOTP)
+
+        phoneNumberEditText = findViewById(R.id.editTextPhoneNumber)
+        getOtpButton = findViewById(R.id.buttonGetOtp)
+        otpEditText = findViewById(R.id.editTextOTP)
         verifyButton = findViewById(R.id.buttonVerify)
 
-        phoneNumber.addTextChangedListener(object : TextWatcher {
+        phoneNumberEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(
                 s: CharSequence?,
                 start: Int, count: Int, after: Int
@@ -40,9 +55,9 @@ class SignupScreen2 : AppCompatActivity() {
             ) {
                 s?.apply {
                     if (RegistrationUtils.isValidPhoneNumber(s.toString())) {
-                        phoneNumber.error = null
+                        phoneNumberEditText.error = null
                     } else {
-                        phoneNumber.error = "erroe"
+                        phoneNumberEditText.error = "error"
                     }
                 }
             }
@@ -53,12 +68,88 @@ class SignupScreen2 : AppCompatActivity() {
         })
 
 
-        verifyButton.setOnClickListener(
+        getOtpButton.setOnClickListener(
             object : View.OnClickListener {
                 override fun onClick(v: View) {
-                    startActivity(intent)
-                    finish()
+                    if (phoneNumberEditText.text.toString().trim().isEmpty()) {
+                        Toast.makeText(applicationContext, "Enter mobile", Toast.LENGTH_SHORT)
+                            .show()
+                        return
+                    }
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        "+91" + phoneNumberEditText.text.toString(),
+                        60,
+                        TimeUnit.SECONDS,
+                        this@SignupScreen2,
+                        object : OnVerificationStateChangedCallbacks() {
+                            override fun onCodeSent(
+                                verificationId: String,
+                                forceResendingToken: ForceResendingToken
+                            ) {
+                                super.onCodeSent(verificationId, forceResendingToken)
+                                verificationOTP = verificationId
+                            }
+
+                            override fun onVerificationCompleted(phoneAuthCredential: PhoneAuthCredential) {
+                            }
+
+                            override fun onVerificationFailed(e: FirebaseException) {
+                                Toast.makeText(applicationContext, e.message, Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    )
                 }
+            }
+        )
+        verifyButton.setOnClickListener(
+            object : View.OnClickListener {
+                override fun onClick(p0: View?) {
+                    if (otpEditText.text.toString().isEmpty()) {
+                        Toast.makeText(applicationContext, "Please enter Otp", Toast.LENGTH_SHORT)
+                            .show()
+                        return
+                    }
+                    if (otpEditText.text.toString().length != 6) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Please enter valid Otp",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        return
+                    }
+                    var otp = otpEditText.text.toString()
+                    if (verificationOTP != null) {
+                        var phoneAuthCredential: PhoneAuthCredential =
+                            PhoneAuthProvider.getCredential(
+                                verificationOTP,
+                                otp
+                            )
+                        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential)
+                            .addOnCompleteListener(
+                                object : OnCompleteListener<AuthResult> {
+                                    override fun onComplete(task: Task<AuthResult>) {
+                                        if (task.isSuccessful) {
+                                            startActivity(
+                                                Intent(
+                                                    applicationContext,
+                                                    SignupScreen1::class.java
+                                                )
+                                            )
+                                        } else {
+                                            Toast.makeText(
+                                                applicationContext,
+                                                "OTP is invalid",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                    }
+
+                                }
+                            )
+                    }
+                }
+
             }
         )
 
