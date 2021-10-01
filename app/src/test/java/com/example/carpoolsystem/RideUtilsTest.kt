3635846abe95d5db2efb8eaeeb64e08b814c7dd1
Kritika sharma <kritika.sharma_cs19@gla.ac.in>
2021-10-01 23:36:59 +0530

    create testcases for RideUtilsTest.kt

--- app/src/test/java/com/example/carpoolsystem/RideUtilsTest.kt
+++ app/src/test/java/com/example/carpoolsystem/RideUtilsTest.kt
@@ -1,5 +1,49 @@
 package com.example.carpoolsystem
 
+import org.junit.Assert
 import org.junit.Assert.*
+import org.junit.Test
 
-class RideUtilsTest
+class RideUtilsTest{
+    @Test
+    fun isValidSourceContainOnlySpecialCharacters_False() {
+        val result = RideUtils.isValidSource("+;'.")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidSourceContainOnlyDigits_False() {
+        val result = RideUtils.isValidSource("1235678")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidSourceContainSpaces_False() {
+        val result = RideUtils.isValidSource("  ")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidSourceIsBlank_False() {
+        val result = RideUtils.isValidSource("")
+        Assert.assertFalse(result)
+    }
+
+    @Test
+    fun isValidDestinationContainOnlySpecialCharacters_False() {
+        val result = RideUtils.isValidDestination("+;'.")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidDestinationContainOnlyDigits_False() {
+        val result = RideUtils.isValidDestination("1235678")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidDestinationContainSpaces_False() {
+        val result = RideUtils.isValidDestination("  ")
+        Assert.assertFalse(result)
+    }
+    @Test
+    fun isValidDestinationIsEmpty_False() {
+        val result = RideUtils.isValidDestination("")
+        Assert.assertFalse(result)
+    }
+}
