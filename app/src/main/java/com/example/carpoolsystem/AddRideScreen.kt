a7ddf51680aa96a8dfb308ad7be9f59a052f0b48
deepanshi_mitta <deepanshi.mittal_cs19@gla.ac.in>
2021-10-03 11:46:49 +0530

    Adding Email Verification in SignupScreen1.kt

--- app/src/main/java/com/example/carpoolsystem/AddRideScreen.kt
+++ app/src/main/java/com/example/carpoolsystem/AddRideScreen.kt
@@ -1,130 +1,142 @@
 package com.example.carpoolsystem
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.lang.NullPointerException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddRideScreen : AppCompatActivity() {
     private val SOURCE_ERROR = "invalid source format"
     private val DESTINATION_ERROR = "invalid destination format"
-    private val DATE_ERROR = "invalid date format"
     private val TIME_ERROR = "invalid time format"
-
-
     private lateinit var source: EditText
     private lateinit var destination: EditText
     private lateinit var time: EditText
-    private lateinit var date: EditText
     private lateinit var addRide: TextView
     private lateinit var addDetails: Button
+    private lateinit var tvDatePicker:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_ride_screen)
-        source=findViewById(R.id.editTextSource)
-        destination=findViewById(R.id.editTextDestination)
-        time=findViewById(R.id.editTextTime)
-        date=findViewById(R.id.editTextDate)
-        addRide=findViewById(R.id.textViewAddRide)
-        addDetails=findViewById(R.id.buttonSubmit)
+        source = findViewById(R.id.editTextSource)
+        destination = findViewById(R.id.editTextDestination)
+        time = findViewById(R.id.editTextTime)
+        addRide = findViewById(R.id.textViewAddRide)
+        addDetails = findViewById(R.id.buttonSubmit)
+        try {
+            tvDatePicker = findViewById(R.id.date)
+        }
+        catch( e:NullPointerException ){
 
-        source.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(
-                s: CharSequence?,
-                start: Int, count: Int, after: Int
-            ) {
+        }
+            val cal = Calendar.getInstance()
+            val myYear = cal.get(Calendar.YEAR)
+            val myMonth = cal.get(Calendar.MONTH)
+            val day = cal.get((Calendar.DAY_OF_MONTH))
+            tvDatePicker.setOnClickListener {
+                val datePickerDialog = DatePickerDialog(
+                    this,
+                    DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                        tvDatePicker.text = "Date:" + dayOfMonth + "/" + (month + 1) + "/" + year
 
+                    },
+                    myYear,
+                    myMonth,
+                    day
+                )
+                datePickerDialog.show()
             }
 
-            override fun onTextChanged(
-                s: CharSequence?,
-                start: Int, before: Int, count: Int
-            ) {
-                s?.apply {
-                    if (RideUtils.isValidSource(s.toString())) {
-                        source.error = null
-                    } else {
-                        source.error = SOURCE_ERROR
+
+
+
+            source.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int, count: Int, after: Int
+                ) {
+
+                }
+
+                override fun onTextChanged(
+                    s: CharSequence?,
+                    start: Int, before: Int, count: Int
+                ) {
+                    s?.apply {
+                        if (RideUtils.isValidSource(s.toString())) {
+                            source.error = null
+                        } else {
+                            source.error = SOURCE_ERROR
+                        }
                     }
                 }
-            }
 
-            override fun afterTextChanged(s: Editable?) {
+                override fun afterTextChanged(s: Editable?) {
 
-            }
-        })
+                }
+            })
 
-        destination.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(
-                p0: CharSequence?,
-                p1: Int, p2: Int, p3: Int
-            ) {
-            }
+            destination.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    p0: CharSequence?,
+                    p1: Int, p2: Int, p3: Int
+                ) {
+                }
 
-            override fun onTextChanged(
-                p0: CharSequence?,
-                p1: Int, p2: Int, p3: Int
-            ) {
-                p0?.apply {
-                    if (RideUtils.isValidDestination(p0.toString())) {
-                        destination.error = null
-                    } else {
-                        destination.error = DESTINATION_ERROR
+                override fun onTextChanged(
+                    p0: CharSequence?,
+                    p1: Int, p2: Int, p3: Int
+                ) {
+                    p0?.apply {
+                        if (RideUtils.isValidDestination(p0.toString())) {
+                            destination.error = null
+                        } else {
+                            destination.error = DESTINATION_ERROR
+                        }
                     }
                 }
-            }
 
-            override fun afterTextChanged(p0: Editable?) {}
-        })
+                override fun afterTextChanged(p0: Editable?) {}
+            })
 
-        date.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(
-                p0: CharSequence?,
-                p1: Int, p2: Int, p3: Int
-            ) {
-            }
 
-            override fun onTextChanged(
-                p0: CharSequence?,
-                p1: Int, p2: Int, p3: Int
-            ) {
-                p0?.apply {
-                    if (RideUtils.isValidDate(p0.toString())) {
-                        date.error = null
-                    } else {
-                        date.error = DATE_ERROR
+            time.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    p0: CharSequence?,
+                    p1: Int, p2: Int, p3: Int
+                ) {
+                }
+
+                override fun onTextChanged(
+                    p0: CharSequence?,
+                    p1: Int, p2: Int, p3: Int
+                ) {
+                    p0?.apply {
+                        if (RideUtils.isValidTime(p0.toString())) {
+                            time.error = null
+                        } else {
+                            time.error = TIME_ERROR
+                        }
                     }
                 }
-            }
 
-            override fun afterTextChanged(p0: Editable?) {}
-        })
+                override fun afterTextChanged(p0: Editable?) {}
+            })
 
-        time.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(
-                p0: CharSequence?,
-                p1: Int, p2: Int, p3: Int
-            ) {
-            }
 
-            override fun onTextChanged(
-                p0: CharSequence?,
-                p1: Int, p2: Int, p3: Int
-            ) {
-                p0?.apply {
-                    if (RideUtils.isValidTime(p0.toString())) {
-                     time.error = null
-                    } else {
-                        time.error = TIME_ERROR
-                    }
-                }
-            }
+        }
 
-            override fun afterTextChanged(p0: Editable?) {}
-        })
+
+
 
 }
-}
+
