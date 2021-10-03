eb58fa5152a25b8c3cc045a74da88650c05c5c93
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-10-03 12:43:30 +0530

    Fix error in building apk

--- app/src/main/java/com/example/carpoolsystem/SignupScreen1.kt
+++ app/src/main/java/com/example/carpoolsystem/SignupScreen1.kt
@@ -1,13 +1,17 @@
 package com.example.carpoolsystem
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SignupScreen1 : AppCompatActivity() {
+    private var firebaseAuth: FirebaseAuth? = null
     private val USERNAME_ERROR = "invalid Name"
     private val PASSWORD_ERROR = "invalid password"
     private val EMAIL_ID_ERROR = "invalid emailId"
@@ -15,14 +19,21 @@ class SignupScreen1 : AppCompatActivity() {
     private lateinit var password: EditText
     private lateinit var emailId: EditText
     private lateinit var name: EditText
+    private lateinit var buttonNext: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_screen1)
 
+        firebaseAuth = FirebaseAuth.getInstance()
         password = findViewById(R.id.editTextPassword)
         emailId = findViewById(R.id.editTextEmail)
-        name=findViewById(R.id.editTextName)
+        name = findViewById(R.id.editTextName)
+        buttonNext = findViewById(R.id.buttonNext)
+
+        buttonNext.setOnClickListener {
+            register()
+        }
 
         password.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(
@@ -65,6 +76,7 @@ class SignupScreen1 : AppCompatActivity() {
             ) {
                 p0?.apply {
                     if (RegistrationUtils.isValidEmail(p0.toString())) {
+
                         emailId.error = null
                     } else {
                         emailId.error = EMAIL_ID_ERROR
@@ -96,11 +108,38 @@ class SignupScreen1 : AppCompatActivity() {
 
             override fun afterTextChanged(p0: Editable?) {}
         })
+    }
+
+    fun checkEmail() {
+        val firebaseUser = firebaseAuth?.currentUser
+        firebaseUser?.sendEmailVerification()?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "Verification mail send", Toast.LENGTH_SHORT).show()
+                firebaseAuth?.signOut()
+                finish()
+                startActivity(Intent(this, SignupScreen2PhoneNumber::class.java))
+            } else {
+                Toast.makeText(this, "Error Occured", Toast.LENGTH_SHORT).show()
+            }
+        }
 
 
     }
+
+    fun register() {
+        var e: String = emailId.text.toString()
+        var p: String = password.text.toString()
+        if (e.isEmpty() || p.isEmpty()) {
+            Toast.makeText(this, "Fill all fields", Toast.LENGTH_SHORT).show()
+        } else {
+            firebaseAuth?.createUserWithEmailAndPassword(e, p)?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    checkEmail()
+                } else {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
 }
-
-
-
-
