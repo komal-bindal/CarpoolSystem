296997042d442903a2c0294b9d8a70ad8faddf27
Kritika sharma <kritika.sharma_cs19@gla.ac.in>
2021-10-06 16:44:46 +0530

    Reformated the xml code

---
+++ app/src/main/java/com/example/carpoolsystem/screens/EntranceLayout.kt
@@ -0,0 +1,11 @@
+package com.example.carpoolsystem.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EntranceLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entrance_layout)
+    }
+}
