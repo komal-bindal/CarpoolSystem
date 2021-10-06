49bed3b13ffeeb8fdd9d7eccd249b27313aebbfc
Kritika sharma <kritika.sharma_cs19@gla.ac.in>
2021-10-06 16:50:15 +0530

    Changed the set Content View --->R.layout.activity_entrance_layout

--- app/src/main/java/com/example/carpoolsystem/screens/EntranceLayout.kt
+++ app/src/main/java/com/example/carpoolsystem/screens/EntranceLayout.kt
@@ -2,6 +2,7 @@ package com.example.carpoolsystem.screens
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.carpoolsystem.R
 
 class EntranceLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
