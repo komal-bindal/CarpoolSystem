6ba3384a0a6f8ba12b5428e87d72a46268515741
Kritika sharma <kritika.sharma_cs19@gla.ac.in>
2021-10-10 03:46:10 +0530

    add UI for cgange car details, change password, drivers profile, passenger profile, no results found

---
+++ app/src/main/java/com/example/carpoolsystem/screens/DriversProfile.kt
@@ -0,0 +1,12 @@
+package com.example.carpoolsystem.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.carpoolsystem.R
+
+class DriversProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drivers_profile)
+    }
+}
