ea8356ee5522f2ba97999273dde8e3f7d8bfbcc0
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-11-14 21:59:18 +0530

    fix the files to merge in main branch

--- app/src/main/java/com/example/carpoolsystem/screens/MainActivity.kt
+++ app/src/main/java/com/example/carpoolsystem/screens/MainActivity.kt
@@ -9,11 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent=Intent(this@MainActivity,EntranceLayout::class.java)
+        val intent = Intent(this@MainActivity, CarNumber::class.java)
 
         startActivity(intent)
         finish()
-
-
     }
 }
