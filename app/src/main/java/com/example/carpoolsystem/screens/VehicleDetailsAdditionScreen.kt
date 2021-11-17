15bc9abc6c81245a619d96e4595971d8564c9812
deepanshi_mitta <deepanshi.mittal_cs19@gla.ac.in>
2021-11-17 12:16:03 +0530

    Completed car number functionality

--- app/src/main/java/com/example/carpoolsystem/screens/VehicleDetailsAdditionScreen.kt
+++ app/src/main/java/com/example/carpoolsystem/screens/VehicleDetailsAdditionScreen.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -11,11 +13,12 @@ import com.example.carpoolsystem.R
 import com.example.carpoolsystem.utility.VehicleUtils
 
 class VehicleDetailsAdditionScreen : AppCompatActivity() {
-    private val CAR_NUMBER_ERROR = "invalid number format"
     private val CAR_MODEL_ERROR = "invalid model format"
     private val CAR_MAKE_ERROR = "invalid cake make format"
     private val LICENCE_NUMBER_ERROR = "invalid number format"
-    private lateinit var carNumber: Button
+    lateinit var letters: EditText
+    lateinit var digits: EditText
+    lateinit var districtCode: EditText
     private lateinit var carModel: EditText
     private lateinit var licenceNumber: EditText
     private lateinit var carMake: EditText
@@ -23,16 +26,116 @@ class VehicleDetailsAdditionScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vehicle_details_addition_screen)
-        carNumber = findViewById(R.id.buttonCarNumber3)
+        letters = findViewById(R.id.letters)
+        digits = findViewById(R.id.digits)
+        districtCode = findViewById(R.id.districtcode)
         carModel = findViewById(R.id.editTextnewCarModel)
         licenceNumber = findViewById(R.id.editTextLicenseNumber)
         carMake = findViewById(R.id.editTextnewCarMake)
         saveDetails = findViewById(R.id.buttonsavenewdetails)
-        carNumber.setOnClickListener {
-            val intent = (Intent(this@VehicleDetailsAdditionScreen, CarNumber::class.java))
-            startActivity(intent)
-            finish()
-        }
+        districtCode.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                p0: CharSequence?,
+                p1: Int, p2: Int, p3: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                p0: CharSequence?,
+                p1: Int, p2: Int, p3: Int
+            ) {
+                p0?.apply {
+                    if (VehicleUtils.isValidDistrictCode(p0.toString())) {
+                        districtCode.error = null
+                    } else {
+                        districtCode.error ="Enter two digit district Code"
+                    }
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+        letters.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                p0: CharSequence?,
+                p1: Int, p2: Int, p3: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                p0: CharSequence?,
+                p1: Int, p2: Int, p3: Int
+            ) {
+                p0?.apply {
+                    if (VehicleUtils.isValidLetters(p0.toString())) {
+                        letters.error = null
+                    } else {
+                        letters.error ="Enter 2 Valid Capital Letters"
+                    }
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+        digits.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                p0: CharSequence?,
+                p1: Int, p2: Int, p3: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                p0: CharSequence?,
+                p1: Int, p2: Int, p3: Int
+            ) {
+                p0?.apply {
+                    if (VehicleUtils.isValidDigits(p0.toString())) {
+                        digits.error = null
+                    } else {
+                        digits.error ="Enter four digit Valid Number"
+                    }
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+
+        var textView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView4)
+        val array = arrayOf<String>(
+            "UP",
+            "UK",
+            "CG",
+            "HR",
+            "PB",
+            "AN",
+            "BR",
+            "AP",
+            "AR",
+            "AS",
+            "DL",
+            "GA",
+            "GJ",
+            "JH",
+            "KA",
+            "KL",
+            "LA",
+            "LD",
+            "MP",
+            "MH",
+            "ML",
+            "MZ",
+            "NL",
+            "OD",
+            "PY",
+            "RG",
+            "WB",
+            "TN",
+            "Others",
+
+            )
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, array)
+        textView.setAdapter(arrayAdapter)
+
         carModel.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(
                 p0: CharSequence?,
