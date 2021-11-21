8fef5cc48c17da3a368f84785ab67911806f56b7
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-11-21 19:37:07 +0530

    complete driver profile with change and add phone number and email

--- app/src/main/java/com/example/carpoolsystem/screens/DriversProfile.kt
+++ app/src/main/java/com/example/carpoolsystem/screens/DriversProfile.kt
@@ -17,12 +17,13 @@ class DriversProfile : AppCompatActivity() {
     private val PHONE_NUMBER = "phoneNumber"
     private val UID = "uId"
     private val USER = "user"
-    private lateinit var phonenumberChangeDriver: TextView
-    private lateinit var nameDriver: TextView
-    private lateinit var passwordChangeDriver: TextView
-    private lateinit var logoutDriver: Button
-    private lateinit var feedbackForm: TextView
-    private lateinit var emailIdDriver: TextView
+    private lateinit var phonenumberChangeDriverTextView: TextView
+    private lateinit var nameDriverTextView: TextView
+    private lateinit var passwordChangeDriverTextView: TextView
+    private lateinit var logoutDriverButton: Button
+    private lateinit var feedbackFormTextView: TextView
+    private lateinit var emailIdDriverTextView: TextView
+    private lateinit var driverPhoneNumberTextView: TextView
     private var FEEDBACK_URL = "https://forms.gle/2zD9AoMWGegbTwmg8"
 
 
@@ -30,64 +31,74 @@ class DriversProfile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drivers_profile)
 
-        phonenumberChangeDriver = findViewById(R.id.phonenumberdriver)
-        passwordChangeDriver = findViewById(R.id.passwordchangedriver)
-        logoutDriver = findViewById(R.id.buttonlogoutdriver)
-        feedbackForm = findViewById(R.id.feedbackForm)
-        emailIdDriver = findViewById(R.id.emailofDriver)
-        nameDriver = findViewById(R.id.fullNameDriver)
+        phonenumberChangeDriverTextView = findViewById(R.id.phonenumberdriver)
+        passwordChangeDriverTextView = findViewById(R.id.passwordchangedriver)
+        logoutDriverButton = findViewById(R.id.buttonlogoutdriver)
+        feedbackFormTextView = findViewById(R.id.feedbackForm)
+        emailIdDriverTextView = findViewById(R.id.emailofDriver)
+        nameDriverTextView = findViewById(R.id.fullNameDriver)
+        driverPhoneNumberTextView = findViewById(R.id.driverPhoneNumber)
 
-        passwordChangeDriver.setOnClickListener {
+        fetchDetailsFromDatabase()
+
+        passwordChangeDriverTextView.setOnClickListener {
             val intent = Intent(this@DriversProfile, ChangePassword::class.java)
             startActivity(intent)
         }
 
-        feedbackForm.setOnClickListener {
+        feedbackFormTextView.setOnClickListener {
             var uri = Uri.parse(FEEDBACK_URL)
             startActivity(Intent(Intent.ACTION_VIEW, uri))
         }
 
-        logoutDriver.setOnClickListener {
+        logoutDriverButton.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             val intent = Intent(this@DriversProfile, UsersScreen::class.java)
             startActivity(intent)
             finish()
         }
 
-        phonenumberChangeDriver.setOnClickListener {
-            val intent = Intent(this@DriversProfile, SignInScreen::class.java)
+        phonenumberChangeDriverTextView.setOnClickListener {
+            val intent = Intent(this, AddNewPhoneNumber::class.java)
+            if (phonenumberChangeDriverTextView.text.toString().equals("Add phone number")) {
+                intent.putExtra("Phone", "add")
+            } else {
+                intent.putExtra("phone", "change")
+            }
             startActivity(intent)
+            finish()
         }
 
-        emailIdDriver.setOnClickListener {
-            if (emailIdDriver.text.toString().equals("Add your email id")) {
-                val intent = Intent(this@DriversProfile, EmailIdEmpty::class.java)
+        emailIdDriverTextView.setOnClickListener {
+            if (emailIdDriverTextView.text.toString().equals("Add your email id")) {
+                val intent = Intent(this, EmailIdEmpty::class.java)
                 startActivity(intent)
+                finish()
             }
         }
+    }
 
+    private fun fetchDetailsFromDatabase() {
         val firebaseUser = FirebaseAuth.getInstance().currentUser
-        val docReference = FirebaseFirestore.getInstance().collection(USERS_COLLECTION)
-            .whereEqualTo(USER, firebaseUser?.uid)
-        docReference.get().addOnSuccessListener { querySnapshot ->
+        val docRef = FirebaseFirestore.getInstance().collection(USERS_COLLECTION)
+            .whereEqualTo(UID, firebaseUser?.uid.toString())
+        docRef.get().addOnSuccessListener { querySnapshot ->
             if (!querySnapshot.isEmpty) {
                 var list = querySnapshot.documents
                 for (item in list) {
-                    val name = item?.data?.get(NAME).toString()
-                    val emailId = item?.data?.get(EMAIL_ID).toString()
-                    val phoneNumber = item?.data?.get(PHONE_NUMBER).toString()
-                    val uId = item?.data?.get(UID).toString()
-                    val user = item?.data?.get(USER).toString()
-                    if (!name.isEmpty()) {
-                        nameDriver.text = name
+                    val userName = item.data?.get("name").toString()
+                    val emailId = item.data?.get("emailId").toString()
+                    val phoneNumber = item.data?.get("phoneNumber").toString()
+                    if (!userName.isEmpty()) {
+                        nameDriverTextView.text = userName
+                    }
+                    if (emailId.isEmpty() == false) {
+                        emailIdDriverTextView.text = emailId
+                    }
+                    if (phoneNumber.isEmpty() == false) {
+                        driverPhoneNumberTextView.text = phoneNumber
                     } else {
-                        nameDriver.text = user.uppercase()
-                    }
-                    if (!emailId.isEmpty()) {
-                        emailIdDriver.text = emailId
-                    }
-                    if (!phoneNumber.isEmpty()) {
-                        phonenumberChangeDriver.text = emailId
+                        passwordChangeDriverTextView.text = "Add phone number"
                     }
                 }
             }
