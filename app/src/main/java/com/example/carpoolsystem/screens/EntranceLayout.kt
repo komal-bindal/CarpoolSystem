c06534614e14bb4e084763d7d1f52b7319f463d7
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-11-22 21:59:34 +0530

    send the user to dashboard when application opens if he is already logged in and the app is not running in background

--- app/src/main/java/com/example/carpoolsystem/screens/EntranceLayout.kt
+++ app/src/main/java/com/example/carpoolsystem/screens/EntranceLayout.kt
@@ -3,8 +3,11 @@ package com.example.carpoolsystem.screens
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.carpoolsystem.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class EntranceLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +15,37 @@ class EntranceLayout : AppCompatActivity() {
         setContentView(R.layout.activity_entrance_layout)
         supportActionBar?.hide()
         Handler().postDelayed({
-            val intent = Intent(this@EntranceLayout, UsersScreen::class.java)
-            startActivity(intent)
-            finish()
+            val currentUser = FirebaseAuth.getInstance().currentUser
+            Log.d("current", currentUser?.uid.toString())
+            if (!currentUser?.uid.toString().isEmpty() && currentUser?.uid != null) {
+                val docRef = FirebaseFirestore.getInstance().collection("users")
+                    .whereEqualTo("uid", currentUser.uid.toString())
+                docRef.get().addOnSuccessListener { query ->
+                    if (!query.isEmpty) {
+                        for (item in query.documents) {
+                            val user = item.data?.get("user").toString()
+                            if (user == "Driver") {
+                                val intent = Intent(this@EntranceLayout, Dashboard::class.java)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                val intent =
+                                    Intent(this@EntranceLayout, DashboardPassenger::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                        }
+                    } else {
+                        val intent = Intent(this@EntranceLayout, UsersScreen::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            } else {
+                val intent = Intent(this@EntranceLayout, UsersScreen::class.java)
+                startActivity(intent)
+                finish()
+            }
         }, 3000)
     }
 }
