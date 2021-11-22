3639d571dae6cf742928698dda3c1741dcaa1cd4
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-11-22 21:04:43 +0530

    add locations in database and implement location review functionality

--- app/src/main/java/com/example/carpoolsystem/screens/MapActivity.kt
+++ app/src/main/java/com/example/carpoolsystem/screens/MapActivity.kt
@@ -8,6 +8,8 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.carpoolsystem.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MapActivity : AppCompatActivity() {
 
@@ -23,15 +25,27 @@ class MapActivity : AppCompatActivity() {
         pickupPoint = findViewById(R.id.autocompleteTextViewsource)
         dropPoint = findViewById(R.id.autocompleteTextViewdestination)
 
-        val pickupLocations = resources.getStringArray(R.array.locations)
+        val locations = arrayListOf<String>("")
         val pickupArrayAdapter =
-            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, pickupLocations)
+            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, locations)
         pickupPoint.setAdapter(pickupArrayAdapter)
 
-        val dropLocations = resources.getStringArray(R.array.locations)
         val dropArrayAdapter =
-            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dropLocations)
+            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, locations)
         dropPoint.setAdapter(dropArrayAdapter)
+
+        FirebaseFirestore.getInstance().collection("locations").get()
+            .addOnSuccessListener { querySnapshot ->
+                if (!querySnapshot.isEmpty) {
+                    val list = querySnapshot.documents
+                    for (item in list) {
+                        locations.add(item.data?.get("location").toString())
+                    }
+                }
+            }
+
+
+
 
         pool.setOnClickListener {
             val dropLoc = dropPoint.text.toString()
@@ -49,11 +63,36 @@ class MapActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
             } else {
+                if (!locations.contains(dropLoc)) {
+                    addLocationForReview(dropLoc)
+                    Toast.makeText(
+                        this,
+                        "drop location is added for review since it is not present in our database",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                if (!locations.contains(pickUpLoc)) {
+                    addLocationForReview(pickUpLoc)
+                    Toast.makeText(
+                        this,
+                        "pickup location is added for review since it is not present in our database",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
                 val intent = Intent(this@MapActivity, AddRideScreen::class.java)
                 intent.putExtra("dropPoint", dropPoint.text.toString())
                 intent.putExtra("pickUpPoint", pickupPoint.text.toString())
                 startActivity(intent)
             }
         }
+    }
+
+    private fun addLocationForReview(loc: String) {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val locationHashMap = hashMapOf<String, String>(
+            "uid" to currentUser?.uid.toString(),
+            "location" to loc
+        )
+        FirebaseFirestore.getInstance().collection("ReviewLocations").add(locationHashMap)
     }
 }
