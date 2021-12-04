eb50b9236a6c46d6972e82629056ab2a95f9aa4b
deepanshi_mitta <deepanshi.mittal_cs19@gla.ac.in>
2021-12-04 19:49:49 +0530

    User class

---
+++ app/src/main/java/com/example/carpoolsystem/screens/User.kt
@@ -0,0 +1,3 @@
+package com.example.carpoolsystem.screens
+
+data class User(var drivername:String?=null,var vehiclenumber:String ?=null,var sourcedestination:String ?=null,var date:String ?=null,var time:String ?=null)
