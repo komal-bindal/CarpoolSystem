129a87903974510dfa60e454b2cd46be9ffeb849
deepanshi_mitta <deepanshi.mittal_cs19@gla.ac.in>
2021-12-04 19:48:15 +0530

    linking DriversViewRequest Screen

--- app/src/main/java/com/example/carpoolsystem/screens/DriversViewRequestScreen.kt
+++ app/src/main/java/com/example/carpoolsystem/screens/DriversViewRequestScreen.kt
@@ -6,14 +6,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.carpoolsystem.R
 import com.example.carpoolsystem.RecyclerAdapter
-import com.example.carpoolsystem.screens.model.UsersClass
+import com.example.carpoolsystem.screens.User
+import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 
-class DriversViewRequestScreen : AppCompatActivity() {
+public class DriversViewRequestScreen : AppCompatActivity() {
     private var layoutManager:RecyclerView.LayoutManager?=null
     private var adapter:RecyclerView.Adapter<RecyclerAdapter.ViewHolder>?=null
     private lateinit var recyclerview:RecyclerView
-    private lateinit var userArrayList:ArrayList<UsersClass>
+    private lateinit var userArrayList:ArrayList<User>
     private lateinit var db:FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +26,16 @@ class DriversViewRequestScreen : AppCompatActivity() {
         recyclerview.layoutManager=layoutManager
         adapter=RecyclerAdapter(userArrayList)
         recyclerview.adapter=adapter
+        db= FirebaseFirestore.getInstance();
+        EventChangeListener()
     }
+
+    private fun EventChangeListener() {
+        db.collection("DriversViewRequest")
+            //.addSnapshotListener( new EventListener<QuerySnapshot>(){
+
+           // })
+    }
+
+
 }
