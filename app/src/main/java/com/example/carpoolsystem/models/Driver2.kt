b7eb5d22474f1b793efc4f57bfe47ec6286d094a
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-12-08 21:23:56 +0530

    code refactoring

--- app/src/main/java/com/example/carpoolsystem/models/Driver2.kt
+++
@@ -1,6 +0,0 @@
-package com.example.carpoolsystem.models
-
-data class Driver2(
-    val name: String,
-    val source: String, val destination: String, val date: String, val time: String
-)
--- app/src/main/java/com/example/carpoolsystem/screens/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.carpoolsystem.screens
-
-data class User(var Drivername:String?=null,var VehicleNo:String ?=null,var sourceDestination:String ?=null,var Date:String ?=null,var Time:String ?=null)
